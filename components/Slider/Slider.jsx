6f9d90e978f51a454932074632631e27f83b5997
Bruno Neves <brunoneves741@gmail.com>
2023-04-11 20:26:46 +0100

    fixed testimonials text responsive behavior

--- components/Slider/Slider.jsx
+++ components/Slider/Slider.jsx
@@ -1,14 +1,17 @@
-'use client';
+"use client";
 
-const Slider = ({phrase, member}) => {
+const Slider = ({ phrase, member }) => {
   return (
     <div className="text-center">
-      <p className="text-[1.2rem] max-w-5xl italic mb-5">{phrase}</p>
+      <p className="text-[1.2rem] min-w-[450px] sm:max-w-5xl italic mb-5">
+        {phrase}
+      </p>
 
-
-      <p className="text-[1.2rem] text-blue-500 font-bold uppercase">{member}</p>
+      <p className="text-[1.2rem] text-blue-500 font-bold uppercase">
+        {member}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
